bd1b842186c8acb57b4c4dc669a30324d8096182
erick.jbf <erick@DESKTOP-1G01AG4.localdomain>
2023-10-27 20:31:24 -0300

    feat: correção da feature de divisão

--- calculadora.js
+++ calculadora.js
@@ -1,11 +1,3 @@
-<<<<<<< HEAD
-var args = process.argv.slice(2);
-var x = args[0];
-var y = args[2];
-var operator = args[1];
-function evaluate(param1, param2, operator) {
-return eval(param1 + operator + param2);
-=======
 const soma = () => {
     const num1 = parseInt(args[1]);
     const num2 = parseInt(args[2]);
@@ -26,6 +18,16 @@ const sub = () => {
     }
 };
 
+const div = () => {
+    const num1 = parseInt(args[1]);
+    const num2 = parseInt(args[2]);
+    if (!isNaN(num1) && !isNaN(num2)) {
+        console.log(num1 / num2);
+    } else {
+        console.log('Os argumentos não são números válidos.');
+    }
+};
+
 const args = process.argv.slice(2);
 
 switch (args[0]) {
@@ -36,9 +38,11 @@ switch (args[0]) {
     case 'sub':
         sub();
         break;
+    
+    case 'div':
+        div();
+        break;
         
     default:
         console.log('Operação não suportada:', args[0]);
->>>>>>> parent of 05996da... Merge pull request #1 from Erickjbf/funcionalidades
 }
-if ( console.log( evaluate(x, y, operator) ) ) {}

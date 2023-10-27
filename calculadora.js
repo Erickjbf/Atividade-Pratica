5d5ea04550ea256361c1926e2d21f8a3f725c186
erick.jbf <erick@DESKTOP-1G01AG4.localdomain>
2023-10-27 18:42:26 -0300

    feat: Nova versao do calculo de soma

--- calculadora.js
+++ calculadora.js
@@ -1,2 +1,6 @@
-const args = process.argv.slice(2);
-console.log(parseInt(args[0]) + parseInt(args[1]))
+const soma = () => {
+    console.log(parseInt(args[0]) + parseInt(args[1]));
+    };
+    const args = process.argv.slice(2);
+    soma();
+    

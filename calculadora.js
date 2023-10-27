41320e1d343bb4c99b8761e3752c0efad0ea1f05
erick.jbf <erick@DESKTOP-1G01AG4.localdomain>
2023-10-27 19:01:10 -0300

    feat: Correção de indentação do codigo

--- calculadora.js
+++ calculadora.js
@@ -1,22 +1,23 @@
 const soma = () => {
     console.log(parseInt(args[0]) + parseInt(args[1]));
-    };
-    const sub = () => {
+};
+const sub = () => {
     console.log(parseInt(args[0]) - parseInt(args[1]));
-    }
-    const args = process.argv.slice(2);
+}
+const args = process.argv.slice(2);
     
-    switch (args[0]) {
+switch (args[0]) {
     
     case 'soma':
         soma();
     break;
-    
+        
     case 'sub':
         sub();
     break;
-    
+        
     default:
-        console.log('does not support', arg[0]);
-    }
+        console.log('does not support', args[0]);
+}
+
         
